94b55c7f7a79e5aff2615a8df42b832776832780
Jakob Mattsson <jakob.mattsson@gmail.com>
2012-06-26 14:55:39 +0200

    Updated jsontool

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ jitsu deploy -r patch || {
   echo >&2 "Deployment failed, aborting."
   exit 1
 }
-VERSION=`cat package.json | json -a -C "version"`
+VERSION=`cat package.json | json -a -A "version"`
 git add package.json
 git commit -m $VERSION
 git push
