6282152957b7e950efedf2b4facd637bbc79250b
Jakob Mattsson <jakob.mattsson@gmail.com>
2012-08-11 16:02:38 +0200

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,8 @@ jitsu deploy -r patch || {
   echo >&2 "Deployment failed, aborting."
   exit 1
 }
-VERSION=`cat package.json | json -a -A "version"`
+VERSION=`cat package.json | json -aC "version"`
 git add package.json
 git commit -m $VERSION
 git push
+exit 0 # ensures we exit with a code of 0, even if the commit doesn't do anything
